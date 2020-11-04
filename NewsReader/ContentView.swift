c622e7e4e5c7d7a628a51c3a9171433a397cd0ba
notynote <32334875+notynote@users.noreply.github.com>
2020-11-04 20:17:46 +0700

    Added basic layout and basic structure of post

--- NewsReader/ContentView.swift
+++ NewsReader/ContentView.swift
@@ -9,13 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(posts) { post in
+                Text(post.title)
+            }
+            .navigationTitle("Hacker News")
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 12")
     }
 }
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+    
+}
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "World"),
+    Post(id: "3", title: "Dee"),
+    Post(id: "4", title: "Ja")
+]
